e9b968821978856475c63c31454ca74535439501
Rodrigo Blanc <digblanc@gmail.com>
2017-05-26 11:53:13 -0300

    Corrigido upload de imagem

--- showcase-web/src/main/java/br/com/vitrinedecristal/controller/ProductController.java
+++ showcase-web/src/main/java/br/com/vitrinedecristal/controller/ProductController.java
@@ -1,8 +1,14 @@
 package br.com.vitrinedecristal.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.ServletContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -11,13 +17,16 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
-import com.sun.jersey.multipart.FormDataParam;
-
+import br.com.vitrinedecristal.dto.ImageDTO;
 import br.com.vitrinedecristal.enums.ProductStatusEnum;
 import br.com.vitrinedecristal.exception.BusinessException;
 import br.com.vitrinedecristal.exception.EntityNotFoundException;
@@ -44,6 +53,12 @@ public class ProductController extends SpringBeanAutowiringSupport {
 
 	@Autowired
 	private IImageService imageService;
+
+	@Context
+	private ServletContext context;
+
+	// private static final String UPLOAD_FOLDER = "C:/blanc/jboss-as-7.1.1.Final/standalone/deployments/showcase-web.war/assets/upload";
+	private static final String UPLOAD_FOLDER = "C:/blanc/workspace/showcase/showcase-web/src/main/webapp/assets/upload/";
 
 	@GET
 	@Path("/{id}")
@@ -126,30 +141,85 @@ public class ProductController extends SpringBeanAutowiringSupport {
 
 	@POST
 	@Path("{id}/image")
-	@Consumes({
-			"application/x-www-form-urlencoded",
-			MediaType.MULTIPART_FORM_DATA
-	})
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	@ApiOperation(value = "Upload de imagem", notes = "Faz o upload das imagens do produto.")
 	@ApiResponses(value = {
 			@ApiResponse(code = 400, message = BadRequestException.MESSAGE, response = ApiExceptionResponse.class),
 			@ApiResponse(code = 403, message = AuthorizationException.MESSAGE, response = ApiExceptionResponse.class)
 	})
-	public void uploadImage(@ApiParam @PathParam("id") Long id, @FormDataParam("file") InputStream imageInputStream) throws BusinessException {
-		// try {
-		/*
-		 * ByteArrayOutputStream out = new ByteArrayOutputStream(); int read = 0; byte[] bytes = new byte[1024]; while ((read = uploadedInputStream.read(bytes)) != -1) { out.write(bytes, 0, read); }
-		 */
-		// byte[] bytes = IOUtils.toByteArray(uploadedInputStream);
-		this.imageService.createImage(id, imageInputStream);
+	// public void uploadImage(@ApiParam @PathParam("id") Long id, @FormDataParam("file") InputStream uploadedStream) throws BusinessException, IOException {
+	public void uploadImage(@ApiParam @PathParam("id") Long id, MultipartFormDataInput multipart) throws BusinessException, IOException {
+		String inputNameFileIndex = "file_id";
+		Integer fileIndex = multipart.getFormDataPart(inputNameFileIndex, Integer.class, null);
 
-		// entityToMerge.setTestBlob(out.toByteArray());
-		// super.edit(entityToMerge);
-		// } catch (IOException e) {
-		// e.printStackTrace();
-		// }
+		String inputNameFileContent = "file";
+		Map<String, List<InputPart>> uploadForm = multipart.getFormDataMap();
+		List<InputPart> inputParts = uploadForm.get(inputNameFileContent);
 
-		// return Response.status(200).entity("uploadFile is called, Uploaded file name : " + fileName).build();
+		this.createDirectory(id);
+
+		for (InputPart inputPart : inputParts) {
+			// Retrieve headers, read the Content-Disposition header to obtain the original name of the file
+			@SuppressWarnings("unused")
+			MultivaluedMap<String, String> header = inputPart.getHeaders();
+
+			String filename = id + File.separator + new Date().getTime() + ".jpg";
+
+			// Handle the body of that part with an InputStream
+			InputStream uploadedStream = inputPart.getBody(InputStream.class, null);
+			this.writeFile(uploadedStream, filename);
+
+			this.imageService.createImage(id, filename);
+		}
+	}
+
+	@DELETE
+	@Path("/{id}/image")
+	@ApiOperation(value = "Remove a imagem", notes = "Remove a imagem do produto.")
+	@ApiResponses(value = {
+			@ApiResponse(code = 400, message = BadRequestException.MESSAGE, response = ApiExceptionResponse.class),
+			@ApiResponse(code = 403, message = AuthorizationException.MESSAGE, response = ApiExceptionResponse.class),
+			@ApiResponse(code = 404, message = EntityNotFoundException.MESSAGE, response = ApiExceptionResponse.class)
+	})
+	public void removeImage(@ApiParam @PathParam("id") Long id) throws BusinessException {
+		this.imageService.removeImage(id);
+	}
+
+	@GET
+	@Path("/{id}/image")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Lista as imagens do produto", notes = "Lista as imagens cadastradas do produto.")
+	@ApiResponses(value = {
+			@ApiResponse(code = 400, message = BadRequestException.MESSAGE, response = ApiExceptionResponse.class),
+			@ApiResponse(code = 403, message = AuthorizationException.MESSAGE, response = ApiExceptionResponse.class)
+	})
+	public List<ImageDTO> listImages(@ApiParam @PathParam("id") Long id) throws ApiException, BusinessException {
+		if (id == null) {
+			throw new EmptyRequestBodyException();
+		}
+
+		return this.imageService.listImagesByProduct(id);
+	}
+
+	private void createDirectory(Long productId) {
+		File directory = new File(UPLOAD_FOLDER + productId);
+		if (!directory.exists()) {
+			directory.mkdir();
+		}
+	}
+
+	private void writeFile(InputStream uploadedStream, String filename) throws IOException, BusinessException {
+		File file = new File(UPLOAD_FOLDER + filename);
+		FileOutputStream fop = new FileOutputStream(file);
+
+		int read = 0;
+		byte[] bytes = new byte[1024];
+		while ((read = uploadedStream.read(bytes)) != -1) {
+			fop.write(bytes, 0, read);
+		}
+
+		fop.flush();
+		fop.close();
 	}
 
 }
